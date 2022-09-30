e2216708162ee30b6da579b3d7d401a9c799765e
atamakonkurii <morisudesu@gmail.com>
2022-09-30 21:40:39 +0900

    huskyが効く様になったので際コミット

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,52 +4,52 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    project: './tsconfig.json',
-    sourceType: 'module',
+    project: "./tsconfig.json",
+    sourceType: "module",
   },
   plugins: [
-    '@typescript-eslint',
-    'jest',
-    'prettier',
-    'prefer-arrow',
-    'react',
-    'react-hooks',
+    "@typescript-eslint",
+    "jest",
+    "prettier",
+    "prefer-arrow",
+    "react",
+    "react-hooks",
   ],
   root: true,
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+        extensions: [".js", "jsx", ".ts", ".tsx"],
       },
     },
   },
   rules: {
     // eslint official
-    'newline-before-return': 'error',
-    'no-console': 'warn',
-    'require-yield': 'error',
+    "newline-before-return": "error",
+    "no-console": "warn",
+    "require-yield": "error",
 
     // @typescript-eslint
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    indent: 'off',
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    indent: "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
 
     // prefer-arrow
-    'prefer-arrow/prefer-arrow-functions': [
-      'error',
+    "prefer-arrow/prefer-arrow-functions": [
+      "error",
       {
         disallowPrototype: true,
         singleReturnOnly: true,
@@ -58,30 +58,30 @@ module.exports = {
     ],
 
     // react
-    'react/jsx-filename-extension': [
-      'error',
+    "react/jsx-filename-extension": [
+      "error",
       {
-        extensions: ['jsx', 'tsx'],
+        extensions: ["jsx", "tsx"],
       },
     ],
-    'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    'react/prop-types': 'off',
-    'react/prefer-stateless-function': 'off',
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/prop-types": "off",
+    "react/prefer-stateless-function": "off",
 
     // react hooks
-    'react-hooks/rules-of-hooks': 'error',
+    "react-hooks/rules-of-hooks": "error",
 
     // prettier
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
         bracketSpacing: true,
         printWidth: 80,
         semi: true,
         singleQuote: true,
-        trailingComma: 'all',
+        trailingComma: "all",
         useTabs: false,
       },
     ],
