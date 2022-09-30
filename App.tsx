9a794593658a6cd4e837fc39b9cba56c6cd3d234
atamakonkurii <morisudesu@gmail.com>
2022-09-30 22:48:42 +0900

    cssが当たらない

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import tw from "tailwind-rn";
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { Hello } from "./component/Hello";
 
 export default function App() {
   return (
-    <View style={tw("flex-1 bg-gray-300 justify-center items-center")}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider utilities={utilities}>
+      <Hello />
+    </TailwindProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
